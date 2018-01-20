7ae49335815c49417aff4330f0cf98afbc877252
Tamizhvendan S <tamizh88@gmail.com>
2018-01-20 11:52:25 +0530

    adds ascii doc styles

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
 $(function(){
   $(".dropdown-menu a").attr('target', '_blank');
+  $("b.conum").each(function(i, e){
+    var $e = $(e)
+    var content = $e.html().replace("(","").replace(")","")
+    $e.html(content)
+    $span = $e.children('span')
+    $span.attr('style','color:#fff !important')
+  })
+  $(".colist").children('ol').addClass('custom-counter')
 });
