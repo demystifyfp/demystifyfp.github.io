f0e524b0c1e7a79281d11099ea5f1ff7c43516da
Tamizhvendan S <tamizh88@gmail.com>
2019-07-05 19:46:14 +0530

    publish

--- js/custom.js
+++ js/custom.js
@@ -8,10 +8,18 @@ $(function () {
     $span.attr('style', 'color:#fff !important')
   })
   $(".colist").children('ol').addClass('custom-counter')
-  $("code.language-clojure").each(function (i, e) {
-    var level = 0;
-    $(e).children("span.token.punctuation").each(function (i, t) {
-      console.log($(t).html())
-    })
+
+  var level = 0
+  Prism.hooks.add('wrap', function (env) {
+    if (env.language == "clojure" && env.type == "punctuation") {
+      if (env.content == "(" || env.content == "[" || env.content == "{") {
+        level++;
+        env.classes.push("rbl" + level);
+      }
+      if (env.content == ")" || env.content == "]" || env.content == "}") {
+        env.classes.push("rbl" + level);
+        level--;
+      }
+    }
   });
 });
