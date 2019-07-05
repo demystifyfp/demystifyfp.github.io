551d89838d8badc421006d365453cfc701a391a1
Tamizhvendan S <tamizh88@gmail.com>
2019-07-05 19:43:32 +0530

    publish

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,17 @@
-$(function(){
+$(function () {
   $("a").attr('target', '_blank');
-  $("b.conum").each(function(i, e){
+  $("b.conum").each(function (i, e) {
     var $e = $(e)
-    var content = $e.html().replace("(","").replace(")","")
+    var content = $e.html().replace("(", "").replace(")", "")
     $e.html(content)
     $span = $e.children('span')
-    $span.attr('style','color:#fff !important')
+    $span.attr('style', 'color:#fff !important')
   })
   $(".colist").children('ol').addClass('custom-counter')
+  $("code.language-clojure").each(function (i, e) {
+    var level = 0;
+    $(e).children("span.token.punctuation").each(function (i, t) {
+      console.log($(t).html())
+    })
+  });
 });
